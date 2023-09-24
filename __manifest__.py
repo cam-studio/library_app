6d99b790866e466ce1eb02a73809654390a3ca56
yan-kuan <yenwu.tw@gmail.com>
2023-09-24 11:47:35 +0000

    update app info

--- __manifest__.py
+++ __manifest__.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "library_app",
+    'name': "Library Management",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Manage library catelog and book lending.""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Vinci Coding",
+    'license': "AGPL-3",
+    'website': "http://www.davinci-universe.com/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Services/Library',
+    'version': '15.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
+
+    # Display in the app list or not
+    'application': True,
 
     # always loaded
     'data': [
